163b0ceb8d9c2b08275e5a92fdcb2a3732fc850f
Nishant Baranwal <nishant.july@gmail.com>
2020-02-10 23:59:04 +0530

    First Commit

---
+++ app/src/main/java/com/theavengers/movieapp/SplashScreenActivity.kt
@@ -0,0 +1,37 @@
+package com.theavengers.movieapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    var compositeDisposable:CompositeDisposable = CompositeDisposable()
+
+
+    override fun onResume() {
+        super.onResume()
+        compositeDisposable.add(Observable.timer(2,TimeUnit.SECONDS).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread()).subscribe {
+                val intent = Intent(applicationContext,LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        compositeDisposable.dispose()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
