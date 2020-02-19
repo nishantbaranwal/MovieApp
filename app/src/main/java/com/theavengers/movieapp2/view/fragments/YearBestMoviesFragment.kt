fe244545f92e3a13048893d810e2a76cf4b6dff4
Nishant Baranwal <nishant.july@gmail.com>
2020-02-19 09:51:40 -0500

    Working Code

--- app/src/main/java/com/theavengers/movieapp2/view/fragments/YearBestMoviesFragment.kt
+++ app/src/main/java/com/theavengers/movieapp2/view/fragments/YearBestMoviesFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,7 +15,7 @@ import com.theavengers.movieapp2.R
 import com.theavengers.movieapp2.model.ResultList
 import com.theavengers.movieapp2.repository.api.TheMovieDbApiInterface
 import com.theavengers.movieapp2.viewmodel.getRetrofit
-import com.theavengers.movieapp2.viewmodel.TopMoviesAdapter
+import com.theavengers.movieapp2.viewmodel.MoviesAdapter
 import io.reactivex.Single
 import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -28,6 +29,13 @@ class YearBestMoviesFragment(context: Context) : Fragment() {
     var disposable: Disposable? = null
     var resultList: ResultList? = null
     var fragmentView: View? = null
+    var isScrolling: Boolean = false
+    var currentItems:Int = 0
+    var totalItems:Int = 0
+    var scrollItems:Int = 0
+    var pageNumber : Int = 1
+    var newDataList: ResultList?= ResultList()
+    var adapter:MoviesAdapter?= null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,7 +48,7 @@ class YearBestMoviesFragment(context: Context) : Fragment() {
             getRetrofit()
                 ?.create(TheMovieDbApiInterface::class.java)
         val responseSingleResultList: Single<Response<ResultList>>? =
-            theMovieDbApiInterface?.getYearTopDramas()
+            theMovieDbApiInterface?.getYearTopDramas(pageNumber)
         val progressDialog = ProgressDialog(ctx)
 
         progressDialog.max = 100
@@ -73,18 +81,78 @@ class YearBestMoviesFragment(context: Context) : Fragment() {
         return fragmentView
     }
 
-    fun updateUI(resultList: ResultList) {
+    fun updateUI(resultList: ResultList){
         val recyclerView: RecyclerView? = fragmentView?.findViewById(R.id.recyclerView)
         val layoutManager = LinearLayoutManager(ctx)
 
         recyclerView?.setLayoutManager(layoutManager)
         recyclerView?.setHasFixedSize(true)
 
-        val adapter = TopMoviesAdapter(
+        adapter = MoviesAdapter(
             resultList.resultList as ArrayList<ResultList.Results>,
             ctx
         )
         recyclerView?.adapter = adapter
+
+
+        recyclerView?.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                    isScrolling = true
+                }
+
+            }
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                currentItems = layoutManager?.childCount!!
+                totalItems = layoutManager?.itemCount!!
+                scrollItems = layoutManager?.findFirstVisibleItemPosition()!!
+                if(isScrolling && (currentItems+scrollItems) == totalItems){
+                    isScrolling = false
+                    fetchData()
+                }
+            }
+        })
+
+
+    }
+
+    private fun fetchData() {
+        pageNumber++
+        val theMovieDbApiInterface : TheMovieDbApiInterface? = getRetrofit()
+            ?.
+                create(TheMovieDbApiInterface::class.java)
+        val responseSingleResultList : Single<Response<ResultList>>? = theMovieDbApiInterface?.
+            getYearTopDramas(pageNumber)
+        val progressDialog = ProgressDialog(ctx)
+//        progressDialog.max = 100
+//        progressDialog.setMessage("Its loading....")
+//        progressDialog.setTitle("Downloading")
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+        progressDialog.show()
+
+        responseSingleResultList?.subscribeOn(Schedulers.io())?.observeOn(
+            AndroidSchedulers.mainThread()
+        )?.subscribe(object : SingleObserver<Response<ResultList>> {
+
+            override fun onSuccess(t: Response<ResultList>) {
+
+                newDataList = t.body();
+                resultList?.resultList?.addAll(newDataList?.resultList!!)
+                Log.d("SizeASDAD",resultList?.resultList?.size.toString())
+                adapter?.addNewData(newDataList?.resultList)
+                progressDialog.dismiss()
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                disposable = d
+            }
+
+            override fun onError(e: Throwable) {
+                progressDialog.dismiss()
+            }
+
+        })
     }
 
     override fun onDestroy() {
